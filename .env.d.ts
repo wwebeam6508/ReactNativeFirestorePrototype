f7d6769fbe8064fa9f1d99e3718f8026d33f2171
Wim <policebeam@gmail.com>
2022-04-19 21:18:02 +0700

    h

---
+++ .env.d.ts
@@ -0,0 +1,12 @@
+
+declare module '@env' {
+    export const REACT_APP_API_KEY:string
+    export const REACT_APP_AUTH_DOMAIN:string
+    export const REACT_APP_DATA_BASE_URL:string
+    export const REACT_APP_PROJECT_ID:string
+    export const REACT_APP_STORAGE_BUCKET:string
+    export const REACT_APP_MESSAGING_SENDER_ID:string
+    export const REACT_APP_ID:string
+    export const REACT_APP_MEASUREMENT_ID:string
+    export const REACT_APP_MAP_BOX_GL:string
+}
